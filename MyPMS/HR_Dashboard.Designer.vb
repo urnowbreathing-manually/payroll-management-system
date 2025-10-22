cfd18fc0b54a5676af1bf9f9c3da8194e9070e2e
urnowbreathing-manually <andreizantua.ucc1@gmail.com>
2025-10-22 13:15:50 +0100

    Pushed Changes

--- MyPMS/HR_Dashboard.Designer.vb
+++ MyPMS/HR_Dashboard.Designer.vb
@@ -28,6 +28,7 @@ Partial Class HR_Dashboard
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
         Me.Name = "HR_Dashboard"
         Me.Size = New System.Drawing.Size(776, 376)
         Me.ResumeLayout(False)
--- MyPMS/HR_Login.vb
+++ MyPMS/HR_Login.vb
@@ -1,9 +1,6 @@
 ﻿Public Class HR_Login
 
     Public Shared MainContentPanel As Panel
-    Public Shared EmployeeID As String = ""
-    Public Shared Password As String = ""
-
     Public Sub New(Parent As Panel)
         InitializeComponent()
         MainContentPanel = Parent
@@ -11,18 +8,6 @@
 
     Private Sub HR_Login_Load(sender As Object, e As EventArgs) Handles Me.Load
         DateTodayLbl.Text = Date.Now.Date.ToString
-
-    End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
-        EmployeeID &= e.KeyChar
-    End Sub
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
-        Password &= e.KeyChar
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
     End Sub
 
