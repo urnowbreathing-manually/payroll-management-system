aeb1acbb14eecb7b019676355ad967f2d59d0279
Allen <Allen@MSI>
2025-10-26 16:03:36 +0800

    Payroll Records HR Admin

--- MyPMS/PayrollRecordsHRA.Designer.vb
+++ MyPMS/PayrollRecordsHRA.Designer.vb
@@ -22,16 +22,98 @@ Partial Class PayrollRecordsHRA
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.EmployeeName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GrossSalary = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Deduction = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NetSalary = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Payroll Records"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(74, 99)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Search"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(180, 98)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Searchbtn"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.EmployeeName, Me.GrossSalary, Me.Deduction, Me.NetSalary})
+        Me.DataGridView1.Location = New System.Drawing.Point(74, 142)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(498, 379)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'EmployeeName
+        '
+        Me.EmployeeName.HeaderText = "Employee Name"
+        Me.EmployeeName.Name = "EmployeeName"
+        '
+        'GrossSalary
+        '
+        Me.GrossSalary.HeaderText = "Gross Salary"
+        Me.GrossSalary.Name = "GrossSalary"
+        '
+        'Deduction
+        '
+        Me.Deduction.HeaderText = "Deduction"
+        Me.Deduction.Name = "Deduction"
+        '
+        'NetSalary
+        '
+        Me.NetSalary.HeaderText = "Net Salary"
+        Me.NetSalary.Name = "NetSalary"
         '
         'PayrollRecordsHRA
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "PayrollRecordsHRA"
         Me.Size = New System.Drawing.Size(830, 570)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents EmployeeName As DataGridViewTextBoxColumn
+    Friend WithEvents GrossSalary As DataGridViewTextBoxColumn
+    Friend WithEvents Deduction As DataGridViewTextBoxColumn
+    Friend WithEvents NetSalary As DataGridViewTextBoxColumn
 End Class
